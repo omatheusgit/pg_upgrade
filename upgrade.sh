8361f6d3e992f569eca92d1195d49f2f8352d5a7
root <root@2749N.wareline.local>
2025-04-11 10:56:18 -0300

    Subindo ajustes homologados para versão 14 & 16

--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-#Feito por Matheus Rafael - 2023
+#Feito por Matheus Rafael - Wareline 2023
+#Atualizado em 03/2025
 
+#Troca as cor
 green=$(tput setaf 2)
 blue=$(tput setaf 6)
 red=$(tput setaf 1)
 reset=$(tput sgr0)
+
+#Variaveis postgresql.conf
+TOTALM=`free -m | head -n 2 | tail -n 1 | awk {'print $2'}`
 SHABM=`echo $(( TOTALM*25/100 ))`
 ECSM=`echo $(( TOTALM*75/100 ))`
 
@@ -15,6 +20,7 @@ echo -e "\n ${green} ### SCRIPT INICIADO! ### ${reset} \n "
 $oldpost
 $newpost
 $currentDir
+$newdir
             
 
 
@@ -23,9 +29,11 @@ echo -e "\n"
 read -p "${blue}Digite a versão NOVA DESEJADA do PostgreSQL: ${reset}" newpost
 echo -e "\n"
 read -p "${blue}Digite o diretório atual do PostgreSQL: ${reset}" currentDir
+echo -e "\n"
+read -p "${blue}Digite o nome do novo diretório do PostgreSQL: (exemplo: home, dados, banco. SEM O '/' ${reset}" newdir
 
 # Exibir confirmação
-echo -e "\n ${green}Versão antiga: $oldpost \n \n Versão nova: $newpost \n \n Diretório atual: $currentDir ${reset} \n"
+echo -e "\n ${green}Versão antiga: $oldpost \n \n Versão nova: $newpost \n \n Diretório atual: $currentDir \n \n Diretório novo: $newdir  ${reset} \n"
 
 read -p "${blue}Deseja continuar com a configuração? (Y/n): ${reset}" response
 
@@ -48,26 +56,44 @@ sudo apt update &> /dev/null
 # Instala o PostgreSQL
 sudo apt install postgresql-$newpost postgresql-client-$newpost -y &> /dev/null
 
+#Corrigir a falha de novos linux não subir o cluster novo
+echo "${green}Verificando cluster da nova versão $newpost ${reset}"
+
+if pg_lsclusters | grep -q "$newpost.*main.*online"; then
+    echo "${green}Cluster $newpost já está ativo.${reset}"
+else
+    echo "${red}Cluster $newpost não está ativo. Tentando ativar...${reset}"
+
+    pg_createcluster $newpost main --start
+    sleep 10
+    if pg_lsclusters | grep -q "$newpost.*main.*online"; then
+        echo "${green}Cluster $newpost iniciado com sucesso.${reset}"
+    else
+        echo "${red}Falha ao iniciar o cluster $newpost. Saindo do script.${reset}"
+        exit 1
+    fi
+fi
+
 # Etapa 3: Criar pastas e modificar parâmetros
 echo "${green}Criando diretorio postgres$newpost ${reset}"
 # Criar diretórios
-mkdir /home/postgres$newpost &> /dev/null
-mkdir /home/postgres$newpost/data &> /dev/null
+mkdir /$newdir/postgres$newpost &> /dev/null
+mkdir /$newdir/postgres$newpost/data &> /dev/null
 
 # Ajustar proprietário
-chown -R postgres:postgres /home/postgres$newpost &> /dev/null
+chown -R postgres:postgres /$newdir/postgres$newpost &> /dev/null
 
 # Copiar dados do binario padrão para o novo
-cp -Rf /var/lib/postgresql/$newpost/main/* /home/postgres$newpost/data &> /dev/null
+cp -Rf /var/lib/postgresql/$newpost/main/* /$newdir/postgres$newpost/data &> /dev/null
 
 # Navegar até o diretório
-cd /home/postgres$newpost
+cd /$newdir/postgres$newpost/
 
 # Ajustar proprietário novamente
 chown -R postgres:postgres * &> /dev/null
 
 # Voltar ao diretório inicial
-cd /home
+cd /$newdir
 
 # Ajustar permissões
 chmod 700 postgres$newpost -Rf &> /dev/null
@@ -76,7 +102,7 @@ echo -e "\n${green}Configuração novo ambiente PostgreSQL concluída com sucess
 
 #Ajustando configurações no antigo postgreSQL 
 echo "${green}Iniciando configurações do PostgreSQL $oldpost ${reset}"
-/usr/lib/postgresql/$oldpost/bin/psql -d banco -U USERDONO -c "ALTER ROLE postgres WITH SUPERUSER;" &> /dev/null
+/usr/lib/postgresql/$oldpost/bin/psql -d db1 -U PACIENTE -c "ALTER ROLE postgres WITH SUPERUSER;" &> /dev/null
 
 ##Ajustando configurações no novo postgreSQL 
 echo "${green}Iniciando configurações do PostgreSQL $newpost ${reset}"
@@ -90,8 +116,9 @@ sed -i -e "s/#backslash_quote = safe_encoding/backslash_quote = on/" postgresql.
 sed -i -e "s/#standard_conforming_strings = on/standard_conforming_strings = off/" postgresql.conf
 sed -i -e "s/#deadlock_timeout = 1s/deadlock_timeout = 5s/" postgresql.conf
 sed -i -e "s/#max_locks_per_transaction = 64/max_locks_per_transaction = 128/" postgresql.conf
-#sed -i -e "s/shared_buffers = 128MB/shared_buffers = $(( SHABM ))MB/" postgresql.conf            	
-#sed -i -e "s/#effective_cache_size = 4GB/effective_cache_size = $(( ECSM ))MB/" postgresql.conf
+##essas duas abaixo - se der problema no .conf
+sed -i -e "s/shared_buffers = 128MB/shared_buffers = $(( SHABM ))MB/" postgresql.conf            	
+sed -i -e "s/#effective_cache_size = 4GB/effective_cache_size = $(( ECSM ))MB/" postgresql.conf
 sed -i -e "s/max_connections = 100/max_connections = 300/" postgresql.conf
 sed -i -e "s/#synchronous_commit = on/synchronous_commit = off/" postgresql.conf
 sed -i -e "s/#checkpoint_segments = 3/checkpoint_segments = 10/" postgresql.conf
@@ -102,17 +129,17 @@ sed -i -e "s/#logging_collector = off/logging_collector = on/" postgresql.conf
 sed -i -e "s/#log_filename = 'postgresql-%Y-%m-%d_%H%M%S.log'/log_filename = 'postgresql-%a.log'/" postgresql.conf
 sed -i -e "s|log_timezone = 'Etc/UTC'|log_timezone = 'America/Sao_Paulo'|" postgresql.conf
 sed -i -e "s|timezone = 'Etc/UTC'|timezone = 'America/Sao_Paulo'|" postgresql.conf
-sed -i -e "s|data_directory = '/var/lib/postgresql/14/main'|data_directory = '/home/postgres$newpost/data/'|" postgresql.conf
+sed -i -e "s|data_directory = '/var/lib/postgresql/$newpost/main'|data_directory = '/$newdir/postgres$newpost/data/'|" postgresql.conf
 sed -i -e "s|port = 5433 |port = 50432|" postgresql.conf
 sed -i -e "s/#track_activity_query_size = 1024/track_activity_query_size = 5000/" postgresql.conf
 sed -i -e "s/escape_string_warning = off/escape_string_warning = on/" postgresql.conf
 
 set PGCLIENTENCODING=utf-8
-touch /home/postgres$newpost/data/pg_upgrade_server.log &> /dev/null
+touch /$newdir/postgres$newpost/data/pg_upgrade_server.log &> /dev/null
 chown -R postgres:postgres * &> /dev/null
-chown -R postgres:postgres /home/postgres$newpost/data/pg_upgrade_server.log &> /dev/null
+chown -R postgres:postgres /$newdir/postgres$newpost/data/pg_upgrade_server.log &> /dev/null
 chmod 700 * &> /dev/null
-chmod 777 /home/postgres$newpost/data/pg_upgrade_server.log &> /dev/null
+chmod 777 /$newdir/postgres$newpost/data/pg_upgrade_server.log &> /dev/null
 
 #Restart dos serviços postgreSQL Antigos e novos para subir novos clusters
 /etc/init.d/postgresql restart &> /dev/null
@@ -126,9 +153,9 @@ chown -R postgres:postgres postgresql.conf &> /dev/null
 chown -R postgres:postgres conf.d &> /dev/null
 
 echo "${green}Copiando arquivos de configuração PostgreSQL $newpost ${reset}" 
-cp /etc/postgresql/$newpost/main/postgresql.conf /home/postgres$newpost/data/ &> /dev/null
-cp -r /etc/postgresql/$newpost/main/conf.d /home/postgres$newpost/data/ &> /dev/null
-cd /home/postgres$newpost/data/ &> /dev/null
+cp /etc/postgresql/$newpost/main/postgresql.conf /$newdir/postgres$newpost/data/ &> /dev/null
+cp -r /etc/postgresql/$newpost/main/conf.d /$newdir/postgres$newpost/data/ &> /dev/null
+cd /$newdir/postgres$newpost/data/ &> /dev/null
 chown -R postgres:postgres postgresql.conf
 chown -R postgres:postgres conf.d
 
@@ -140,12 +167,12 @@ sudo -u postgres /bin/bash <<EOF
 
 echo "${green}Parando serviços de clusters ${reset}"
 /usr/lib/postgresql/$oldpost/bin/pg_ctl stop -D "$currentDir" &> /dev/null
-/usr/lib/postgresql/$newpost/bin/pg_ctl stop -D "/home/postgres$newpost/data" &> /dev/null
+/usr/lib/postgresql/$newpost/bin/pg_ctl stop -D "/$newdir/postgres$newpost/data" &> /dev/null
 
-cd /home/postgres$newpost/data/
+cd /$newdir/postgres$newpost/data/
 echo "${green} Iniciando processo de upgrade ${reset}"
 echo " ${red} #### POR SEGURANÇA, NÃO STOPAR ESSE PROCESSO #### ${reset}"
-/usr/lib/postgresql/$newpost/bin/pg_upgrade -b "/usr/lib/postgresql/$oldpost/bin" -B "/usr/lib/postgresql/$newpost/bin" -d "$currentDir" -D "/home/postgres$newpost/data" -Upostgres #Aparecer na tela o resultado
+/usr/lib/postgresql/$newpost/bin/pg_upgrade -b "/usr/lib/postgresql/$oldpost/bin" -B "/usr/lib/postgresql/$newpost/bin" -d "$currentDir" -D "/$newdir/postgres$newpost/data" -Upostgres #Aparecer na tela o resultado
 EOF
 
 # Alterando parametros de portas
@@ -158,15 +185,15 @@ sed -i -e "s|port = 5432|port = 5433| " postgresql.conf
 /etc/init.d/postgresql restart &> /dev/null
 
 # Manutenções após finalizar Upgrade
-cd /home/postgres$newpost/data/
+cd /$newdir/postgres$newpost/data/
 echo "${green}Iniciando Vacuum ${reset}"
 #
 echo -e "\n Executar: ${red} /usr/lib/postgresql/$newpost/bin/vacuumdb -U postgres --all --analyse-in-stages ${reset}"
 
 #Opcional após upgrade
-cd /home/postgres$newpost/data
-/usr/lib/postgresql/$newpost/bin/psql -d banco -U USERDONO -f update_extensions.sql &> /dev/null
-/usr/lib/postgresql/$newpost/bin/psql -d banco -U USERDONO -c "ALTER ROLE postgres WITH NOSUPERUSER;" &> /dev/null
+cd /$newdir/postgres$newpost/data
+/usr/lib/postgresql/$newpost/bin/psql -d db1 -U PACIENTE -f update_extensions.sql &> /dev/null
+/usr/lib/postgresql/$newpost/bin/psql -d db1 -U PACIENTE -c "ALTER ROLE postgres WITH NOSUPERUSER;" &> /dev/null
 echo -e " \n \n ${green}Após validações rodar ./delete_old_cluster.sh ${reset}\n "
 
 exit
